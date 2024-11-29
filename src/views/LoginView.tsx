d57944dd98a4f5535986fdbda87053504c335d6a
CarlesVicedo <vicedocarles@gmail.com>
2024-11-29 23:31:16 +0100

    Create auth layout

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom"
 function LoginView() {
     return (
         <>
-            <div className="text-6xl">LoginView</div>
+            <h1 className="text-4xl text-white font-bold">Login</h1>
 
-            <nav>
-                <Link to='/auth/register'>
-                    Create account
-                </Link>
+            <nav className="mt-10">
+                <Link
+                    to='/auth/register'
+                    className="text-center text-white text-lg block"
+                >Create account</Link>
             </nav>
         </>
     )
--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom"
 function RegisterView() {
     return (
         <>
-            <div>RegisterView</div>
+            <h1 className="text-4xl text-white font-bold">Create account</h1>
 
-            <nav>
-                <Link to='/auth/login'>
-                    Log in here
-                </Link>
+            <nav className="mt-10">
+                <Link
+                    to='/auth/login'
+                    className="text-center text-white text-lg block"
+                >Log in here</Link>
             </nav>
         </>
     )
