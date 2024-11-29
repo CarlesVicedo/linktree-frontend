7aa86d0b3db57cd88d02eb034f256223620fc089
CarlesVicedo <vicedocarles@gmail.com>
2024-11-30 00:14:53 +0100

    Creat register form and error message component

--- src/views/RegisterView.tsx
+++ src/views/RegisterView.tsx
@@ -1,9 +1,116 @@
 import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import ErrorMessage from "../components/ErrorMessage"
 
 function RegisterView() {
+
+    const initialValues = {
+        name: '',
+        email: '',
+        handle: '',
+        password: '',
+        password_confirmation: ''
+    }
+
+    const { register, watch, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues })
+
+    console.log(errors)
+
+    const handleRegister = () => {
+        console.log('From handleRegister')
+    }
+
     return (
         <>
             <h1 className="text-4xl text-white font-bold">Create account</h1>
+
+            <form
+                onSubmit={handleSubmit(handleRegister)}
+                className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
+            >
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="name" className="text-2xl text-slate-500">Name</label>
+                    <input
+                        id="name"
+                        type="text"
+                        placeholder="Your name"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('name', {
+                            required: 'Name is mandatory'
+                        })}
+                    />
+
+                    {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+
+                </div>
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Register e-mail"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('email', {
+                            required: 'E-mail is mandatory'
+                        })}
+                    />
+
+                    {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
+
+                </div>
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
+                    <input
+                        id="handle"
+                        type="text"
+                        placeholder="User name without spaces"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('handle', {
+                            required: 'Handle is mandatory'
+                        })}
+                    />
+
+                    {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
+
+                </div>
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Register password"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password', {
+                            required: 'Password is mandatory'
+                        })}
+                    />
+
+                    {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
+
+                </div>
+
+                <div className="grid grid-cols-1 space-y-3">
+                    <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Rpeat Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Repeat Password"
+                        className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password_confirmation', {
+                            required: 'Repeating password is mandatory'
+                        })}
+                    />
+
+                    {errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>}
+
+                </div>
+
+                <input
+                    type="submit"
+                    className="bg-cyan-400 p-3 text-lg w-full uppercase text-slate-600 rounded-lg font-bold cursor-pointer"
+                    value='Create Account'
+                />
+            </form>
 
             <nav className="mt-10">
                 <Link
