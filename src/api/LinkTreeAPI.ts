daf980d0554783c82cb372f3f6ad27d2e4ff4fc2
CarlesVicedo <vicedocarles@gmail.com>
2024-12-02 23:09:01 +0100

    Move my profile to its own component and start using fetched data

--- src/api/LinkTreeAPI.ts
+++ src/api/LinkTreeAPI.ts
@@ -1,12 +1,13 @@
 import { isAxiosError } from "axios"
 import api from "../config/axios"
+import { User } from "../types"
 
 export const getUser = async () => {
    
     try {
-        const { data } = await api('/user', )
+        const { data } = await api<User>('/user')
         return data
-        
+
     } catch (error) {
         if (isAxiosError(error) && error.response) {
          throw new Error(error.response.data.error)
