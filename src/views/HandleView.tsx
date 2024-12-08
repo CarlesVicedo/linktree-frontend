7a6211c8735b85aee71e0a3f970e2f29329a89ef
CarlesVicedo <vicedocarles@gmail.com>
2024-12-08 20:42:03 +0100

    Create component for rendering user data

--- src/views/HandleView.tsx
+++ src/views/HandleView.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 import { Navigate, useParams } from "react-router-dom"
 import { getUserByHandle } from "../api/LinkTreeAPI"
+import HandleData from "../components/HandleData"
 
 const HandleView = () => {
     const params = useParams()
@@ -15,9 +16,7 @@ const HandleView = () => {
     if (isLoading) return <p className="text-center text-white">Loading...</p>
     if (error) return <Navigate to={'/404'} />
 
-    if (data) return (
-        <div>{data.name}</div>
-    )
+    if (data) return <HandleData data={data} />
 }
 
 export default HandleView
