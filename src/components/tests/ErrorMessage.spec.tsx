88797c6ac8001967ae66649519d038dc1c46fddf
CarlesVicedo <vicedocarles@gmail.com>
2025-01-08 13:02:04 +0100

    Add jest configuration and first test

---
+++ src/components/tests/ErrorMessage.spec.tsx
@@ -0,0 +1,17 @@
+// filepath: /c:/Users/carle/Documents/Programacio/linktree/frontend/src/components/ErrorMessage.test.tsx
+import { render, screen } from '@testing-library/react'
+import ErrorMessage from '../ErrorMessage'
+import '@testing-library/jest-dom'
+
+describe('ErrorMessage', () => {
+    it('renders correctly with given children', () => {
+        render(<ErrorMessage>Test Error</ErrorMessage>)
+        expect(screen.getByText('Test Error')).toBeInTheDocument()
+    })
+
+    it('applies correct class names', () => {
+        render(<ErrorMessage>Test Error</ErrorMessage>)
+        const errorMessage = screen.getByText('Test Error')
+        expect(errorMessage).toHaveClass('bg-red-50 text-red-600 p-3 uppercase text-sm font-bold text-center')
+    })
+})
